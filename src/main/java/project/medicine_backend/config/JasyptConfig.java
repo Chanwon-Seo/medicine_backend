dd9c8fdd2e97cf958a8103008b0b4b2d296a93da
Chanwon-Seo <scwonn60@gmail.com>
2022-09-21 16:34:40 +0900

    Jasypt 환경설정 및 구축

--- src/main/java/project/medicine_backend/config/JasyptConfig.java
+++ src/main/java/project/medicine_backend/config/JasyptConfig.java
@@ -1,51 +1,30 @@
 package project.medicine_backend.config;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
-
-@Slf4j
 @Configuration
-@EnableEncryptableProperties //Jasypt
 public class JasyptConfig {
-    @Value("${jasypt.encryptor.algorithm}")
-    private String algorithm;
-    @Value("${jasypt.encryptor.pool-size}")
-    private int poolSize;
-    @Value("${jasypt.encryptor.string-output-type}")
-    private String stringOutputType;
-    @Value("${jasypt.encryptor.key-obtention-iterations}")
-    private int keyObtentionIterations;
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
 
-    @Bean
-    public StringEncryptor jasyptStringEncryptor() {
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        encryptor.setPoolSize(poolSize);
-        encryptor.setAlgorithm(algorithm);
-        encryptor.setPassword(getJasyptEncryptorPassword());
-        encryptor.setStringOutputType(stringOutputType);
-        encryptor.setKeyObtentionIterations(keyObtentionIterations);
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+
+        encryptor.setConfig(config);
 
         return encryptor;
-    }
-
-    private String getJasyptEncryptorPassword() {
-        try {
-            ClassPathResource resource = new ClassPathResource("jasypt-encryptor-password.txt");
-            return Files.readAllLines(Paths.get(resource.getURI())).stream()
-                    .collect(Collectors.joining(""));
-        } catch (IOException e) {
-            throw new RuntimeException("Not found Jasypt password file.");
-        }
     }
 }
