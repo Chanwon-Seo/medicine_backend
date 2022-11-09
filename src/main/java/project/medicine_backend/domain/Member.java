e2208ac0fee668429a5660b0391a6fb435fa2d44
Chanwon-Seo <scwonn60@gmail.com>
2022-11-09 23:12:27 +0900

    post img Update

---
+++ src/main/java/project/medicine_backend/domain/Member.java
@@ -0,0 +1,4 @@
+package project.medicine_backend.domain;
+
+public class Member {
+}
--- src/main/java/project/medicine_backend/web/controller/camera/CameraApiController.java
+++ src/main/java/project/medicine_backend/web/controller/camera/CameraApiController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,37 +22,46 @@ import java.io.IOException;
 @RequestMapping("/camera")
 public class CameraApiController {
 
+    private String keyString;
+
     @GetMapping("/add")
     public String videoGET() {
         log.info("이미지 스트리밍 중입니다.");
         return "video/streamMain";
     }
 
-    @GetMapping("/check")
-    public String videoBy(@RequestParam String path,
-                          @RequestParam String filename) {
-        log.info("인식된 약통 화면입니다. path는 ={}", path);
-        log.info("인식된 약통 화면입니다. filename는 ={}", filename);
-        return "video/ROI";
-    }
-
-    @GetMapping("/img/upload")
-    public String newFile() {
+    //    @GetMapping("/img/upload")
+    public String testimg() {
         return "video/upload-form";
     }
 
     @PostMapping("/img/upload")
-    public ResponseEntity<byte[]> handleFileUpload(@RequestParam("check_img") MultipartFile files) throws IOException {
-
+    public void handleFileUploadV2(@RequestParam("check_img") MultipartFile files) throws IOException {
+        log.info("이미지를 저장해야합니다.");
         ImageVO imageVO = new ImageVO();
         imageVO.setMimetype(files.getContentType());
         imageVO.setOriginal_name(files.getOriginalFilename());
         imageVO.setData(files.getBytes());
-        
+
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", imageVO.getMimetype());
         headers.add("Content-Length", String.valueOf(imageVO.getData().length));
-        return new ResponseEntity<byte[]>(imageVO.getData(), headers, HttpStatus.OK);
+
+        String keyString = Base64Utils.encodeToString(files.getBytes()); //base64로 인코딩한 값
+
+//        Member member = new Member();
+//        member.setKeyString(keyString);
+
+        log.info("keyString@@@@@ ={}", keyString);
     }
 
+    @GetMapping("/testtest/img")
+    public String tesetestset(Model model) {
+
+        log.info("testest keyString={}", keyString);
+        log.info("tesetestset일로 왔음");
+
+        model.addAttribute("fileContent", keyString);
+        return "video/ROI";
+    }
 }
